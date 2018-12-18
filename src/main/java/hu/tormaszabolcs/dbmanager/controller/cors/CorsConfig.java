c710d7511d6ebaff6c37291df5eda19e182fece0
Sztorma <sztorma@outlook.com>
2018-12-18 15:44:52 +0100

    change port to 9000 creating cors config in order to response to frontend

---
+++ src/main/java/hu/tormaszabolcs/dbmanager/controller/cors/CorsConfig.java
@@ -0,0 +1,40 @@
+
+package hu.tormaszabolcs.dbmanager.controller.cors;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    private final String allowOrigin
+            = "http://localhost:8080";
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configAuthentication = new CorsConfiguration();
+        configAuthentication.setAllowCredentials(true);
+        //Changed from this.allowOrigin to "*" (Every domain will be allowed !!!)
+        configAuthentication.addAllowedOrigin("*");
+        configAuthentication.addAllowedHeader("Authorization");
+        configAuthentication.addAllowedHeader("Content-Type");
+        configAuthentication.addAllowedHeader("Accept");
+        configAuthentication.addAllowedMethod("POST");
+        configAuthentication.addAllowedMethod("GET");
+        configAuthentication.addAllowedMethod("DELETE");
+        configAuthentication.addAllowedMethod("PUT");
+        configAuthentication.addAllowedMethod("OPTIONS");
+        configAuthentication.setMaxAge(3600L);
+
+        // source.registerCorsConfiguration("/oauth/token", configAuthentication);
+        source.registerCorsConfiguration("/**", configAuthentication); // Global 
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
+}
